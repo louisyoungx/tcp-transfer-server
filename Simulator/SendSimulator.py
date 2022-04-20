eee94266c04d4185b729cacfe8c7e6870d962cf6
louisyoungx <1462648167@qq.com>
2022-04-21 02:43:43 +0800

    feat: send client with data simulate

---
+++ Simulator/SendSimulator.py
@@ -0,0 +1,42 @@
+import csv
+import socket, threading
+from time import sleep
+
+port = 12000
+server_host = "0.0.0.0"
+simu_data = "simu_data.csv"
+
+def readData():
+    with open(simu_data, "r", encoding='UTF-8') as file:
+        data = csv.reader(file)
+        lst = []
+        for row in data:
+            try:
+                lst.append(row[0])
+            except:
+                pass
+    return lst
+
+def run():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket initialization
+    client.connect((server_host, port))  # connecting client to server
+    client.send("Send".encode())
+    print("start --- >")
+    def write():
+        data = readData()
+        index = 0
+        while True:  # message layout
+            message = data[index]
+            print(message)
+            if index >= len(data):
+                index = 0
+            else:
+                index += 1
+            client.send(message.encode())
+            sleep(0.1)
+
+    write_thread = threading.Thread(target=write)  # sending messages
+    write_thread.start()
+
+if __name__ == '__main__':
+    run()
