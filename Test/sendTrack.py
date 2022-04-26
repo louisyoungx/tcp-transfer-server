8c5093f8560578a250f8c11e452f1b8ebb6cf6b8
louisyoungx <1462648167@qq.com>
2022-04-26 19:43:12 +0800

    feat: test send client

---
+++ Test/sendTrack.py
@@ -0,0 +1,36 @@
+import socket
+import threading  # Libraries import
+
+from Core.WebSocket.WebSocket import WebSocket
+port = 12002
+
+def handleSend(client):
+    while True:
+        try:  # recieving valid messages from client
+            message = client.recv(1024).decode()
+            print(message)
+        except:
+            client.close()
+            print("====== Send Close ======")
+            print('Send Closed')
+            break
+
+def run():  # accepting multiple receivers
+    allow = ''  # LocalHost
+    global receivers
+    receivers = []
+    print("====== Server {} Start ======".format(port))
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket initialization
+    server.bind((allow, port))  # binding limit and port to socket
+    server.listen()
+    client, address = server.accept()
+    key = client.recv(1024)
+    try:
+        value = key.decode()
+        print(value)
+    except:
+        print(f'ERROR MESSAGE: {key}')
+        value = ""
+    print("====== Send Connection Built ======")
+    client.send('Success'.encode())
+    handleSend(client)
